446f4e41df96f3421c686a28340284944911d3a3
Harold <sadcah@yahoo.com>
2023-09-13 18:30:52 +0100

    feat:initial setup

---
+++ app/chatbox/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React, { useRef, useEffect, useState } from 'react';
+
+export default function Chatbox() {
+  return (
+    <main className='container'>
+      <h1>Chatbox Page</h1>
+    </main>
+  );
+}
---
+++ app/homepage/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React, { useRef, useEffect, useState } from 'react';
+
+export default function Homepage() {
+  return (
+    <main className='container'>
+      <h1>Homepage Page</h1>
+    </main>
+  );
+}
