29141a93ca9023129e9ad849772656188e9b7bf3
Harold <sadcah@yahoo.com>
2023-09-15 17:48:21 +0100

    feat:added base users template

---
+++ (components)/Users.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+
+const style = {
+  width: '100%',
+  maxWidth: 360,
+  bgcolor: 'background.paper',
+};
+
+const mockUser = [
+  'user1',
+  'user2',
+  'user3',
+  'user4',
+  'user5',
+  'user6',
+  'user7',
+  'user8',
+  'user9',
+  'user10',
+];
+
+export default function Users() {
+  return (
+    <List sx={style} component='nav' aria-label='users'>
+      {mockUser.map((user, idx) => {
+        return (
+          <>
+            <ListItem button key={idx}>
+              <ListItemText primary={user} />
+            </ListItem>
+            <Divider />
+          </>
+        );
+      })}
+    </List>
+  );
+}
--- app/chatbox/page.tsx
+++ app/chatbox/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
+import Users from '@/(components)/Users';
 import React, { useRef, useEffect, useState } from 'react';
 
 export default function Chatbox() {
   return (
     <main className='container'>
       <h1>Chatbox Page</h1>
+      <Users />
     </main>
   );
 }
